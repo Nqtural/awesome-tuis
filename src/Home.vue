1d6a556b84d76f5572e1bb2d2be1640ebb1f7db9
Nqtural <robin.stalstad@proton.me>
2026-03-01 17:07:55 +0000

    feat(): add video to explain intro/background on main page

--- src/Home.vue
+++ src/Home.vue
@@ -6,7 +6,56 @@ import Sidebar from "./Sidebar.vue";
 	<header>
 		<h1>Awesome TUIs</h1>
 	</header>
-	<main>
-	</main>
-	<Sidebar />
+	<div class="content-wrapper">
+		<main>
+			<iframe
+					width="100%"
+					height="100%"
+					src="https://www.youtube.com/embed/_fLmA4fjiAE"
+					frameborder="0"
+					allow="accelerometer; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+					>
+			</iframe>
+		</main>
+		<div class="sidebar-wrapper">
+			<Sidebar />
+		</div>
+	</div>
 </template>
+
+<style scoped>
+h1 {
+	text-align: center;
+}
+
+.content-wrapper {
+	display: flex;
+	justify-content: center;
+	align-self: center;
+	gap: 2rem;
+	padding: 2rem;
+	box-sizing: border-box;
+	width: 100%;
+}
+
+main {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	height: fit-content;
+	gap: 3rem;
+	flex: 4;
+}
+
+iframe {
+	height: 65%;
+	width: 65%;
+	aspect-ratio: 16 / 9;
+}
+
+.sidebar-wrapper {
+	display: flex;
+	jusify-content: center;
+	flex: 1;
+}
+</style>
